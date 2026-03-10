85e32becf7bb07a3cbb674ba6f272cefef76610b
ankit-cs25mtech11023 <cs25mtech11023@iith.ac.in>
2026-03-10 15:44:37 +0530

    Added boilerplate header, footer, navbar

---
+++ includes/footer.php
@@ -0,0 +1,7 @@
+</div> <footer class="bg-dark text-white text-center py-3 mt-5">
+        <p class="mb-0">&copy; 2026 TransactiWar Project. All rights reserved.</p>
+    </footer>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
